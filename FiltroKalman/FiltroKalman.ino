b954974f094cf042c3b396ce3a22968c65e7e3b0
Gabriel-PC <Gabriel-PC@DESKTOP-E2VDDTU>
2022-05-28 19:28:54 -0300

    ADDED: MPU6050 Library in all projects

--- FiltroKalman/FiltroKalman.ino
+++ FiltroKalman/FiltroKalman.ino
@@ -1,6 +1,7 @@
-#include <MPU6050.h>
 #include <Arduino.h>
 #include <Wire.h>
+#include "MPU6050.h"
+
 #include <math.h>
 #include <Kalman.h>
 
@@ -12,20 +13,26 @@ float tmp=0;
 unsigned long currentTime=0,previousTime=0;
 float ang_accel,vang_gyro,ang_kalman;
 Kalman FiltroKalman;
-
+MPU6050 mpu;
 
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
+  /*Wire.begin();
   Wire.beginTransmission(MPU);
   Wire.write(0x6B);
   Wire.write(0);
-  Wire.endTransmission(true);
+  Wire.endTransmission(true);*/
   FiltroKalman.setQangle(T*T*0.0466);
   FiltroKalman.setQbias(0.0466);
   FiltroKalman.setRmeasure(10);
   FiltroKalman.setAngle(0);
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+
 
 }
 
@@ -34,9 +41,13 @@ void setup() {
 
 void loop() {
   currentTime = micros();
-  if (currentTime-previousTime>=T*1000000)
-  {
-    previousTime=currentTime;
+
+  if (currentTime-previousTime>=T*10000000)
+  {  
+    Vector rawGyro = mpu.readRawGyro();
+    Vector rawAccel = mpu.readRawAccel();
+    
+    /*previousTime=currentTime;
     Wire.beginTransmission(MPU);
     Wire.write(0x3B); // Registrador 0x3B (ACCEL_XOUT_H)
     Wire.endTransmission(false);
@@ -48,18 +59,19 @@ void loop() {
     GyX[1] =(Wire.read() << 8 |Wire.read());
     GyY[1] =(Wire.read() << 8 |Wire.read());
     GyZ[1] =(Wire.read() << 8 |Wire.read());
+    */
     
     //Calcula os ângulos e velocidades ângulares em torno do eixo y
     //ang_accel = atan2(AcX[1], AcZ[1])*180/PI;
     //vang_gyro=GyY[1]/131;
     //Calcula os ângulos e velocidades ângulares em torno do eixo X
-    ang_accel = atan2(AcY[1], AcZ[1])*180/PI;
-    vang_gyro=GyX[1]/131;
+    ang_accel = atan2(rawAccel.YAxis, rawAccel.ZAxis)*180/PI;
+    vang_gyro=rawGyro.XAxis/131;
 
     // Aplica o filtro de Kalman
     ang_kalman = FiltroKalman.getAngle(ang_accel,vang_gyro,T);
-    Serial.print(ang_accel);Serial.print(",");
-    Serial.println(ang_kalman);
+    Serial.print(rawAccel.XAxis/16384);Serial.println(",");
+    //Serial.println(ang_kalman);
     AcX[0]=AcX[1];
     
   }
