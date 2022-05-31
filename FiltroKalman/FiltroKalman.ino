528b2d6aac0b322687fa927f33b6f416e3cd0b4c
Gabriel-PC <Gabriel-PC@DESKTOP-E2VDDTU>
2022-05-31 19:55:49 -0300

    ADDED: Kalman settings for all axies

--- FiltroKalman/FiltroKalman.ino
+++ FiltroKalman/FiltroKalman.ino
@@ -10,30 +10,57 @@
 const double T= 0.005;
 const int MPU = 0x68; //ICD address
 float AcX[2], AcY[2],AcZ[2], Tmp, GyX[2],GyY[2],GyZ[2],Vx[2],Vy[2],Vz[2],Dx=0,Dy=0,Dz=0;// Dados do MPU
-float tmp=0;
 unsigned long currentTime=0,previousTime=0;
-float ang_accel,vang_gyro,ang_kalman;
-Kalman FiltroKalman;
+float ang_accelXZ,vang_gyroY,ang_kalmanXZ,ang_accelYZ,vang_gyroX,ang_kalmanYZ,ang_accelXY,vang_gyroZ,ang_kalmanXY;
+float y_pass_alt[3][3], x_pass_alt[3][3];
+Kalman FiltroKalmanXZ;
+Kalman FiltroKalmanYZ;
+Kalman FiltroKalmanXY;
+Vector rawGyro;
+Vector rawAccel;
 MPU6050 mpu;
 
+void setupKalman()
+{
+  FiltroKalmanXZ.setQangle(T*T*0.0466);
+  FiltroKalmanXZ.setQbias(0.0466);
+  FiltroKalmanXZ.setRmeasure(10);
+  FiltroKalmanXZ.setAngle(0);
+  FiltroKalmanXY.setQangle(T*T*0.0466);
+  FiltroKalmanXY.setQbias(0.0466);
+  FiltroKalmanXY.setRmeasure(10);
+  FiltroKalmanXY.setAngle(0);
+  FiltroKalmanYZ.setQangle(T*T*0.0466);
+  FiltroKalmanYZ.setQbias(0.0466);
+  FiltroKalmanYZ.setRmeasure(10);
+  FiltroKalmanYZ.setAngle(0);
+}
+
+float LPFilterAlternative(float x, int axis)
+{
+  
+  const float a=0.65, b = 0.70;
+  y_pass_alt[2][axis] = (a+b)*y_pass_alt[1][axis] -a*b*y_pass_alt[0][axis] +(1-a-b+a*b)*x_pass_alt[0][axis] ;
+  float y = y_pass_alt[2][axis] ;
+  y_pass_alt[0][axis] =y_pass_alt[1][axis] ;
+  y_pass_alt[1][axis] =y_pass_alt[2][axis] ;
+  x_pass_alt[0][axis] =x_pass_alt[1][axis] ;
+  x_pass_alt[1][axis] =x_pass_alt[2][axis] ;
+  x_pass_alt[2][axis] = x;
+  return y;
+
+}
 
 void setup() {
   Serial.begin(115200);
-  /*Wire.begin();
-  Wire.beginTransmission(MPU);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);*/
-  FiltroKalman.setQangle(T*T*0.0466);
-  FiltroKalman.setQbias(0.0466);
-  FiltroKalman.setRmeasure(10);
-  FiltroKalman.setAngle(0);
+  
+ 
   while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
   {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
   }
-
+  setupKalman();
   mpu.setGyroOffsetX(84);
   mpu.setGyroOffsetY(50);
   mpu.setGyroOffsetZ(84);
@@ -47,44 +74,43 @@ void loop() {
 
   if (currentTime-previousTime>=T*1000000)
   {  
-    Vector rawGyro = mpu.readRawGyro();
-    Vector rawAccel = mpu.readRawAccel();
-    
+    rawGyro = mpu.readRawGyro();
+    rawAccel = mpu.readRawAccel();    
     previousTime=currentTime;
-//    Wire.beginTransmission(MPU);
-//    Wire.write(0x3B); // Registrador 0x3B (ACCEL_XOUT_H)
-//    Wire.endTransmission(false);
-//    Wire.requestFrom(MPU,14,true);
-//    AcX[1] = (Wire.read() << 8 | Wire.read()); 
-//    AcY[1] = (Wire.read() << 8 |Wire.read());
-//    AcZ[1] = (Wire.read() << 8 |Wire.read());
-//    tmp = (Wire.read() << 8 |Wire.read());
-//    GyX[1] =(Wire.read() << 8 |Wire.read());
-//    GyY[1] =(Wire.read() << 8 |Wire.read());
-//    GyZ[1] =(Wire.read() << 8 |Wire.read());
-    
-    
-    //Calcula os ângulos e velocidades ângulares em torno do eixo y
-//    ang_accel = atan2(AcX[1], AcZ[1])*180/PI;
-//    vang_gyro=GyY[1]/131;
-    //Calcula os ângulos e velocidades ângulares em torno do eixo X
-   
-    rawAccel.XAxis=rawAccel.XAxis/16384;
-    accel_fit(&rawAccel.XAxis,0);
-    rawAccel.YAxis=rawAccel.YAxis/16384;
-    accel_fit(&rawAccel.YAxis,1);
-    rawAccel.ZAxis=rawAccel.ZAxis/16384;
-    accel_fit(&rawAccel.ZAxis,2);
-    ang_accel = atan2(rawAccel.XAxis, rawAccel.ZAxis)*180/PI;
-    vang_gyro=rawGyro.YAxis/131;
+
+    fitData();    
+    ang_accelXZ = atan2(rawAccel.XAxis, rawAccel.ZAxis)*180/PI;
+    vang_gyroY=rawGyro.YAxis;
+    ang_accelYZ = atan2(rawAccel.YAxis, rawAccel.ZAxis)*180/PI;
+    vang_gyroX=rawGyro.XAxis;
+    ang_accelXY = atan2(rawAccel.XAxis, rawAccel.YAxis)*180/PI;
+    vang_gyroZ=rawGyro.ZAxis;
     // Aplica o filtro de Kalman
-    ang_kalman = FiltroKalman.getAngle(ang_accel,vang_gyro,T);
+    ang_kalmanXZ = FiltroKalmanXZ.getAngle(ang_accelXZ,vang_gyroY,T);
+    ang_kalmanYZ = FiltroKalmanYZ.getAngle(ang_accelYZ,vang_gyroX,T);
+    ang_kalmanXY = FiltroKalmanXY.getAngle(ang_accelXY,vang_gyroZ,T);
+    // Serial.print(rawAccel.YAxis);Serial.print(",");
+    // Serial.println(rawAccel.YAxis - sin(ang_kalmanYZ*PI/180));
+    AcX[1]=LPFilterAlternative(rawAccel.XAxis,0);
     Serial.print(rawAccel.XAxis);Serial.print(",");
-    Serial.println(rawAccel.XAxis - sin(ang_kalman*PI/180));
+    Serial.println(AcX[1]);
     AcX[0]=AcX[1];
     
   }
   
   // put your main code here, to run repeatedly:
 
+}
+
+void fitData()
+{
+    rawAccel.XAxis=rawAccel.XAxis/16384;
+    accel_fit(&rawAccel.XAxis,0);
+    rawAccel.YAxis=rawAccel.YAxis/16384;
+    accel_fit(&rawAccel.YAxis,1);
+    rawAccel.ZAxis=rawAccel.ZAxis/16384;
+    accel_fit(&rawAccel.ZAxis,2);
+    rawGyro.XAxis=rawGyro.XAxis/131;
+    rawGyro.YAxis=rawGyro.YAxis/131;
+    rawGyro.ZAxis=rawGyro.ZAxis/131;
 }
